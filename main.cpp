1132b59061bc80f124d16bf033bd73012588e059
Sarvagyaagrawal <sarvagyaagrawal@gmail.com>
2020-12-03 19:32:23 +0000

    added insert function level order

--- main.cpp
+++ main.cpp
@@ -1,15 +1,18 @@
 #include <iostream>
-#include<vector>
+#include <vector>
 
 struct Node{
       int info;
       Node *left, *right=NULL;
 };
 Node *start=NULL;
+int count=0;
 
 std::vector<Node *> stack;   //dynamic stack using vectors with structure type Node *
+std::vector<Node *> arr;
 
-// creates a node 
+
+// intialises & returns a node  
 Node *create(int data){
       Node *n=new Node();
       n->info=data;
@@ -17,6 +20,31 @@ Node *create(int data){
     return n;
 }
 
+
+//insertion of node in a binary tree
+void insert(int data){
+      int loc;        //arr loc of parent of the inserting Node
+      Node *n=create(data);
+      
+      arr.push_back(n);
+      if(start==NULL){
+          start=n;
+          count+=1;
+          return;
+      }
+      count+=1;
+      loc=count/2;
+      Node *par=arr[loc];
+      if(par->left==NULL){
+            par->left=n;
+      }
+      else{
+        par->right=n;
+      }
+
+    return;
+}      
+      
 //PreOrder traversal of a given binary tree
 void preorder(Node *root){
       if(root==NULL)
@@ -26,11 +54,11 @@ void preorder(Node *root){
       while(!stack.empty()){
           Node *ptr=stack.back();
           std::cout<<ptr->info<<" ";
-          if(ptr->right!=NULL){
-              stack.push_back(ptr->right);
+          if(ptr->right!=NULL){         
+              stack.push_back(ptr->right);        //check if right child exists
           }
           else if(ptr->left!=NULL){
-            stack.push_back(ptr->left);
+            stack.push_back(ptr->left);       //check if left child exists
           }
           else{
             break;
@@ -40,6 +68,15 @@ void preorder(Node *root){
 return;
 }
 
+
+
 int main() {
-  std::cout<<"";
+  int num;
+  //std::cout<<"enter the data";
+  //std::cin>>num;
+  insert(4);
+  insert(12);
+  insert(23);
+  insert(17);
+  preorder(start);
 }
