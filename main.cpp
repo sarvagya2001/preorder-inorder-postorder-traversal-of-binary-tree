7fa70f66121c50e9dbfe25cf1bde972282052303
Sarvagyaagrawal <sarvagyaagrawal@gmail.com>
2020-12-03 19:00:03 +0000

    added create function

--- main.cpp
+++ main.cpp
@@ -7,24 +7,37 @@ struct Node{
 };
 Node *start=NULL;
 
-std::vector<Node*> stack;
+std::vector<Node *> stack;   //dynamic stack using vectors with structure type Node *
 
+// creates a node 
+Node *create(int data){
+      Node *n=new Node();
+      n->info=data;
+      n->left=n->right=NULL;
+    return n;
+}
+
+//PreOrder traversal of a given binary tree
 void preorder(Node *root){
       if(root==NULL)
           {return;}
       Node *ptr=root;
-      stack.push_back(root);
+      stack.push_back(root);        // push the root to the empty stack
       while(!stack.empty()){
-          ptr=stack.pop_back();
+          Node *ptr=stack.back();
+          std::cout<<ptr->info<<" ";
           if(ptr->right!=NULL){
               stack.push_back(ptr->right);
           }
-          else{
+          else if(ptr->left!=NULL){
             stack.push_back(ptr->left);
+          }
+          else{
+            break;
           }
           
       }
-      
+return;
 }
 
 int main() {
